ef6620ce502fcb1602940f448fe005c45ea06be6
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-02 02:31:20 -0500

    One quick change

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -3,5 +3,15 @@ import axios from "axios";
 const url = "http://jsonplaceholder.typicode.com/todos/1";
 
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data;
+
+  const ID = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+
+  console.log(`
+    The todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
 });
