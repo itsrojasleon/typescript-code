06d4a85bb608e7570cf2ed1a24cc133b952114f9
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-02 02:44:05 -0500

    Catching more errors

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -10,13 +10,17 @@ const url = 'http://jsonplaceholder.typicode.com/todos/1';
 axios.get(url).then(response => {
   const todo = response.data as Todo;
 
-  const ID = todo.id;
+  const id = todo.id;
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
-    The todo with ID: ${ID}
+    The todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${completed}
   `);
-});
+};
