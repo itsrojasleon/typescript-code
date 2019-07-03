700fe2d1bde018d4abfcbb5012727598874b8420
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-03 02:48:53 -0500

    Fixing the Any type

--- features/variables.ts
+++ features/variables.ts
@@ -20,3 +20,14 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20
 };
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
