a0a6604f2d56a77ff4cee6f407d752c38620f984
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-03 02:18:13 -0500

    Object literal annotations

--- features/variables.ts
+++ features/variables.ts
@@ -1,4 +1,22 @@
 let apples: number = 5;
 let speed: string = 'fast';
+let hasName: boolean = true;
 
-apples = 20;
+let nothing: undefined = undefined;
+
+// Built in objects
+const now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let numbers: number[] = [1, 2];
+
+// Classes
+class Car {}
+const car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
