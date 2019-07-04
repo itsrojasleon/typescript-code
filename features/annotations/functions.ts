2ff489014cddc4a49ae9f569098abdebc6a746df
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-04 17:23:24 -0500

    Annotations for Anomymous Functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,15 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+};
