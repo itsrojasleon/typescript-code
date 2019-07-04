9072816cbac8f9b494377d508520d51d942ca127
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-04 17:27:00 -0500

    void and never

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -13,3 +13,11 @@ function divide(a: number, b: number): number {
 const multiply = function(a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
