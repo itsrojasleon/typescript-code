59662a74906652a9f02103297710e14de38ab133
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-04 17:32:33 -0500

    Destructuring with Annotations

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -21,3 +21,20 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 };
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = ({
+  date,
+  weather
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+logWeather(todaysWeather);
