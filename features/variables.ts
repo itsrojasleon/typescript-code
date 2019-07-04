b3eb72e36678d5f3af61f3b27ba968ade0ca74ef
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-04 13:05:52 -0500

    Delayed initialization

--- features/variables.ts
+++ features/variables.ts
@@ -27,7 +27,20 @@ const logNumber: (i: number) => void = (i: number) => {
 };
 
 // When to use annotations
+
 // 1) Function that returns the 'any' type
 const json = '{"x": 10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates); // {x: 10, y: 20}
+
+// 2) When we declare a variable on one line
+// and initalizate it later
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
