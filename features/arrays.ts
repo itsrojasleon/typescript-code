8cf6e7b4340b2763a0f08f48072a36bf4735bcc8
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-05 00:00:01 -0500

    Why typed arrays

--- features/arrays.ts
+++ features/arrays.ts
@@ -1,5 +1,17 @@
 const carMakers = ['ford', 'toyota', 'chevy'];
-
 const dates = [new Date(), new Date()];
-
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+// carMakers.push(100); // ✖️
+
+// Help with 'map'
+carMakers.map(
+  (car: string): string => {
+    return car;
+  }
+);
