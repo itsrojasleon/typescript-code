316caaf7a15502ac51c07c086d8b4a6dbf67a210
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-14 13:48:19 -0500

    Using type definition files

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -7,5 +7,11 @@ class User {
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
 }
