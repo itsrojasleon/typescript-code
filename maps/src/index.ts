432dd94154905c1656d68bc44400fed531767736
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-14 14:36:58 -0500

    Exploring Type Defition Files

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,9 +1,10 @@
-import { User } from './User';
-import { Company } from './Company';
+//import { User } from './User';
+//import { Company } from './Company';
 
-const user = new User();
-
-console.log(user);
-
-const company = new Company();
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
