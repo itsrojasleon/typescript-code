a35dbeb244d69025f4dd33586fa3e56eb002f08c
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-14 13:59:59 -0500

    Export statements in Typescript

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,6 +1,6 @@
 import faker from 'faker';
 
-class User {
+export class User {
   name: string;
   location: {
     lat: number;
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,5 @@
-console.log('Sup');
+import { User } from './User';
+
+const user = new User();
+
+console.log(user);
