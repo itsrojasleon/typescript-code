01a21313741171fea96e735bc7d94cb0f41b80db
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-13 19:47:42 -0500

    Fields with Inheritance

--- features/classes.ts
+++ features/classes.ts
@@ -1,21 +1,28 @@
 class Vehicle {
   constructor(public color: string) {}
+
+  protected honk(): void {
+    console.log('Beep');
+  }
 }
 
 const vehicle = new Vehicle('orange');
 
 console.log(vehicle.color);
 
-// class Car extends Vehicle {
-//   private drive(): void {
-//     console.log('vroom');
-//   }
+class Car extends Vehicle {
+  constructor(public wheels: number, public color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log('vroom');
+  }
 
-//   startDrivingProcess(): void {
-//     this.drive();
-//   }
-// }
+  startDrivingProcess(): void {
+    this.drive();
+  }
+}
 
-// const car = new Car();
+const car = new Car(4, 'red');
 
-// car.startDrivingProcess();
+car.startDrivingProcess();
