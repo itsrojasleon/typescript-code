2f4abccfe71af3b4a3f3acee38a2bb2678ae33fb
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-14 14:06:35 -0500

    Defining a company

---
+++ maps/src/Company.ts
@@ -0,0 +1,18 @@
+import faker from 'faker';
+
+export class Company {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+}
