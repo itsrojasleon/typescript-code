fdc0e6ce6ed69f4a618f26389a20297ae468f80e
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-20 01:27:19 -0500

    Why use private modifiers?

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,5 @@
 //import { User } from './User';
 //import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+new CustomMap('map');
