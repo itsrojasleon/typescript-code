aa3c7256f9053f5699abb1db2c8d142d1a9cc9e9
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-25 02:02:30 -0500

    Showing Popup windows

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -21,12 +21,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there!'
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
