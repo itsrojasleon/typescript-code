5c345dbd735644690a012afbca5a3229449d68b7
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-25 01:52:45 -0500

    Implicit type checks

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,6 +1,3 @@
-import { User } from './User';
-import { Company } from './Company';
-
 // Instructions to every other class
 // on how they can be an argument to 'addMarker'
 interface Mappable {
@@ -32,13 +29,4 @@ export class CustomMap {
       }
     });
   }
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng
-  //     }
-  //   });
-  // }
 }
