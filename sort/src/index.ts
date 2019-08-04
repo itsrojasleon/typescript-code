077940f2a36fd18919ebca7aa3d1d4082a61730e
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-04 01:58:41 -0500

    Sorting Implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,18 @@
 class Sorter {
   constructor(public collection: number[]) {}
-  sort(): void {}
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
 }
 
 const sorter = new Sorter([10, 3, -5, 0]);
