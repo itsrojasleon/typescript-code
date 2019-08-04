9822a4541a024a39360cfb852b8101a8d30051d0
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-04 01:50:58 -0500

    Sorter Scaffolding

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hey 123');
+class Sorter {
+  constructor(public collection: number[]) {}
+  sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
