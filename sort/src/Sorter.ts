29e92a41eefd9550149880ce4871e0b8df9d6dba
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-06 17:37:07 -0500

    Interface definition

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,7 +1,11 @@
-import { NumbersCollection } from './NumbersCollection';
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
 
 export class Sorter {
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: Sortable) {}
   sort(): void {
     const { length } = this.collection;
 
