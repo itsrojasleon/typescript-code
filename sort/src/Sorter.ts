f977b67f98f9eefb87ced1b2239f4acbb79e253c
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-06 16:31:53 -0500

    Extracting key logic

---
+++ sort/src/Sorter.ts
@@ -0,0 +1,17 @@
+export class Sorter {
+  constructor(public collection: ) {}
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+        
+      }
+    }
+  }
+}
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,30 +1,3 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // All of this only works if collection is an array of numbers
-        // If collection is an array of numbers
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-        // Only going to work if collection is a string
-        // If collection is a string, do this logic instead:
-        // ~ Logic to compare and swap characters in a string
-
-        if (typeof this.collection === 'string') {
-        }
-      }
-    }
-  }
-}
-
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
