61ee6ca575669aa7149826f7f60cb4aa76277c05
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-07 17:50:07 -0500

    Runnign an analysis

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,14 @@
-console.log('ok');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map(
+    (row: string): string[] => {
+      return row.split(',');
+    }
+  );
+
+console.log(matches);
