eb77ac39994888b4a39d9a86cd217de7749b53b3
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-07 18:37:06 -0500

    Using enums

--- stats/build/index.js
+++ stats/build/index.js
@@ -12,4 +12,20 @@ var matches = fs_1.default
     .map(function (row) {
     return row.split(',');
 });
-console.log(matches);
+// enum = enumaration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var manUnitedWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -11,4 +11,19 @@ const matches = fs
     }
   );
 
-console.log(matches);
+// enum = enumaration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
