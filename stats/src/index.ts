5e4d03ddc41e7b46ad8b93cdeba0d1866c58841d
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-09 02:36:32 -0500

    Describing a row with a Tuple

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -4,6 +4,8 @@ import { MatchResult } from './MatchResult';
 const reader = new CsvFileReader('football.csv');
 reader.read();
 
+const dateOfFirstMatch = reader.data[0][0];
+
 let manUnitedWins = 0;
 
 for (let match of reader.data) {
@@ -13,3 +15,4 @@ for (let match of reader.data) {
     manUnitedWins++;
   }
 }
+console.log(`Man united won ${manUnitedWins} games`);
