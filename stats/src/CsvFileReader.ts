b2b90086923a8a6fa9d1224596687e07e6e7f26f
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-12 02:05:19 -0500

    Variable Types with Generics

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -3,12 +3,12 @@ import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export abstract class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
 
-  abstract mapRow(row: string[]): MatchData;
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
