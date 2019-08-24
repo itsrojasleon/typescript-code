705e542560c2e5f051c3a919d0f03933ca02884f
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-24 18:31:43 -0500

    One Last Thing

--- stats/src/Summary.ts
+++ stats/src/Summary.ts
@@ -1,4 +1,6 @@
 import { MatchData } from './MatchData';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 export interface Analyzer {
   run(matches: MatchData[]): string;
@@ -8,6 +10,9 @@ export interface OutputTarget {
 }
 
 export class Summary {
+  static winsAnalysisWithHtmlReport(team: string): Summary {
+    return new Summary(new WinsAnalysis(team), new HtmlReport());
+  }
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
 
   buildAndPrintReport(matches: MatchData[]): void {
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -10,6 +10,6 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 summary.buildAndPrintReport(matchReader.matches);
