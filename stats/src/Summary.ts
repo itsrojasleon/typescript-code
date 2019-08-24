bf705e69aa9974f1987a72151961bdb7738158a5
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-24 17:44:18 -0500

    A Composition-Based Approach

---
+++ stats/src/Summary.ts
@@ -0,0 +1,12 @@
+import { MatchData } from './MatchData';
+
+export interface Analyzer {
+  run(matches: MatchData[]): string;
+}
+export interface OutputTarget {
+  print(report: string): void;
+}
+
+export class Summary {
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+}
