e7018ff235b456ea93aee83e4712903ad568115c
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-24 18:08:48 -0500

    Building the Reporter

--- stats/src/Summary.ts
+++ stats/src/Summary.ts
@@ -9,4 +9,9 @@ export interface OutputTarget {
 
 export class Summary {
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -6,14 +6,3 @@ const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-console.log(`Man united won ${manUnitedWins} games`);
