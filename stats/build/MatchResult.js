5bf37fbb78d31e36ea264322c4274f4454aad996
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-24 19:08:25 -0500

    No more dist files

--- stats/build/MatchResult.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// enum = enumaration
-var MatchResult;
-(function (MatchResult) {
-    MatchResult["HomeWin"] = "H";
-    MatchResult["AwayWin"] = "A";
-    MatchResult["Draw"] = "D";
-})(MatchResult = exports.MatchResult || (exports.MatchResult = {}));
--- stats/build/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateStringToDate = function (dateString) {
-    var dateParts = dateString.split('/').map(function (value) {
-        return parseInt(value);
-    });
-    return new Date(dateParts[2], dateParts[1] - 1);
-};
