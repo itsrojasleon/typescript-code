5d2ef712d54db9fa48f9ec83111083172c91831b
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-04 12:17:42 -0500

    More on generics

---
+++ features/generics.ts
@@ -0,0 +1,25 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+new ArrayOfAnything<string>(['a', 'b', 'c']);
