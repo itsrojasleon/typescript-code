74a95d43d273dac1a1177a1aef142de83de34664
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-04 12:28:52 -0500

    Function Generics

--- features/generics.ts
+++ features/generics.ts
@@ -24,3 +24,24 @@ class ArrayOfAnything<T> {
 
 // const arr = new ArrayOfAnything<string>(['a', 'b', 'c']);
 const arr = new ArrayOfAnything(['a', 'b', 'c']);
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+// printAnything<string>(['a', 'b'])
+// printAnything<string>([1, 2]) // Bad
+printAnything(['a', 'b']);
