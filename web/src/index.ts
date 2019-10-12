23660d62aac454c6bc6294a4ab5748648029208c
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-11 19:39:36 -0500

    Dynamic array creation

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,14 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ name: 'newname' });
+user.on('change', () => {
+  console.log('change 1');
+});
+user.on('change', () => {
+  console.log('change 2');
+});
+user.on('save', () => {
+  console.log('save was triggered');
+});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.trigger('sasasave');
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -3,7 +3,7 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => {};
+type Callback = () => void;
 
 export class User {
   events: { [key: string]: Callback[] } = {};
@@ -18,5 +18,19 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback) {}
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.map(cb => cb());
+  }
 }
