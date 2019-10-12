186ecddc8ec786037d225b105ce39dcefc5195e4
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-11 19:57:10 -0500

    Understanding REST Conventions

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,3 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20 });
-
-user.on('change', () => {
-  console.log('change 1');
-});
-user.on('change', () => {
-  console.log('change 2');
-});
-user.on('save', () => {
-  console.log('save was triggered');
-});
-
-user.trigger('sasasave');
+axios.get('http://localhost:3000/users/1');
