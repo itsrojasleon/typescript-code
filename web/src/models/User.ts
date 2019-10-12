51cefd8b50301324e4a9f611cf0e9e9f00d8708e
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-12 18:36:13 -0500

    Refactoring with Composition

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -6,11 +6,7 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -19,22 +15,6 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.map(cb => cb());
   }
 
   fetch(): void {
