04b18d83996a976e221985ebcd64c66d5755c40c
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-15 19:06:25 -0500

    Extending the User

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,7 +1,4 @@
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
-import { Attributes } from './Attributes';
-import { AxiosResponse } from 'axios';
+import { Model } from './Modal';
 
 export interface UserProps {
   id?: number;
@@ -11,41 +8,4 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attibutes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attibutes = new Attributes<UserProps>(attrs);
-  }
-
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attibutes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attibutes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.get('id');
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannont fetch without an id');
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-}
+export class User extends Model<UserProps> {}
