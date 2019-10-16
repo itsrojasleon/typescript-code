27fe5f7795ebd9d59fc2d9828170b5948ffe1681
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-16 13:21:17 -0500

    Generic User

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,10 @@
+import { User, UserProps } from './models/User';
 import { Collection } from './models/Collection';
 
-const collection = new Collection('http://localhost:3000/users');
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3000/users',
+  (json: UserProps) => User.buildUser(json)
+);
 
 collection.on('change', () => {
   console.log(collection);
--- web/src/models/Collection.ts
+++ web/src/models/Collection.ts
@@ -1,12 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
-import { User, UserProps } from './User';
 import { Eventing } from './Eventing';
 
-export class Collection {
-  models: User[] = [];
+export class Collection<T, K> {
+  models: T[] = [];
   events: Eventing = new Eventing();
 
-  constructor(public rootUrl: string) {}
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
   get on() {
     return this.events.on;
@@ -18,9 +17,8 @@ export class Collection {
 
   fetch(): void {
     axios.get(this.rootUrl).then((response: AxiosResponse) => {
-      response.data.map((value: UserProps) => {
-        const user = User.buildUser(value);
-        this.models.push(user);
+      response.data.map((value: K) => {
+        this.models.push(this.deserialize(value));
       });
 
       this.trigger('change');
