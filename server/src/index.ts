33f055713aa0aea91584e0428a34fc146935cae0
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-17 15:19:50 -0500

    Basic Routes with Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hi there!');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hello there!</h1>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on Port 3000');
+});
