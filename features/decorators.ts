4f294763b8b3356677d6e5a7b3c301319c7ae738
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-18 17:22:50 -0500

    Decorators Around Properties

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,17 +1,33 @@
 class Boat {
+  @testDecorator
   color: string = 'red';
 
   get formattedColor(): string {
     return `This boat color is ${this.color}`;
   }
 
-  @testDecorator
+  @logError('Opps boat was sunk in ocean')
   pilot(): void {
-    console.log('Swish');
+    throw new Error();
+    // +console.log('Swish');
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target: ', target);
-  console.log('Key: ', key);
+function testDecorator(target: any, key: string) {
+  console.log(target);
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function(target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function() {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
 }
