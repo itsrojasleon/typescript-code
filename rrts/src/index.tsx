13340b1befd1378711fdd697b2c58d4f5a51f46d
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-21 13:14:35 -0500

    Confusing Component State

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+interface AppProps {
+  color: string;
+}
+
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>Hello there</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        <span>{this.state.counter}</span>
+      </div>
+    );
   }
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App color="red" />, document.getElementById('root'));
