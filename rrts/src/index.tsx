82d94d90902174a0af9106803a537a68618a8766
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-21 13:18:32 -0500

    Functional components

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -5,25 +5,8 @@ interface AppProps {
   color: string;
 }
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        <span>{this.state.counter}</span>
-      </div>
-    );
-  }
-}
 ReactDOM.render(<App color="red" />, document.getElementById('root'));
