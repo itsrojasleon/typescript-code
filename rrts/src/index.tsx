ab01aece91f28881282539cb958b610c0242b34f
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-10-21 12:36:05 -0500

    Simple Components

---
+++ rrts/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return <div>Hello there</div>;
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
